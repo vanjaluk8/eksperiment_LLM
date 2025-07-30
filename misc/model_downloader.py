013f4f42e5af41fdfe13bd1c6a73a47f7336ac36
vluk <vluk@unipu.hr>
2025-07-30 18:03:12 +0200

    tiding up

--- misc/model_downloader.py
+++
@@ -1,7 +0,0 @@
-from huggingface_hub import snapshot_download
-
-snapshot_download(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-    local_dir="/raid/models/hgf_models/Mistral-7B-Instruct-v0.3",
-    local_dir_use_symlinks=False  # ensures actual files, not symlinks
-)
